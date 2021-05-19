1d31f46fea559afaff40bc4a4d50cb89daaddeba
Jelena Feliciano <jfel217@gmail.com>
2021-05-19 14:47:21 -0400

    update

--- src/components/Results.js
+++ src/components/Results.js
@@ -7,13 +7,12 @@ import {
 
 const Results = () => (
     <Container>
-        <h2>Results List</h2>
-        
-        <ListGroup>
-            <ListGroupItem href="#" active>Link 1</ListGroupItem>
-            <ListGroupItem href="#">Link 2</ListGroupItem>
-            <ListGroupItem href="#" disabled>Link 3</ListGroupItem>
-        </ListGroup>
+    <h2>Results List</h2>
+    <ListGroup>
+        <ListGroupItem href="#" active>Link 1</ListGroupItem>
+        <ListGroupItem href="#">Link 2</ListGroupItem>
+        <ListGroupItem href="#" disabled>Link 3</ListGroupItem>
+    </ListGroup>
     </Container>
 );
 
