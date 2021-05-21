a84ad644b8b8b143c22158f2e07a95a7f08791e8
Jelena Feliciano <jfel217@gmail.com>
2021-05-21 12:17:27 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
 test('Renders without crashing', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Search App/i);
-  expect(linkElement).toBeInTheDocument();
+  
 });
 
